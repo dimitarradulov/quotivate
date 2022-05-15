e1108393ccee4df11d3e745fbac568c9897915f9
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-05-15 11:02:47 +0300

    Add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { getAllQuotes } from './store/quote-slice';
 import MainHeader from './components/layout/MainHeader';
 import Notification from './components/UI/Notification';
-import AllQuotes from './pages/AllQuotes';
-import AddQuote from './pages/AddQuote';
-import QuoteDetails from './pages/QuoteDetails';
-import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+const AddQuote = React.lazy(() => import('./pages/AddQuote'));
+const QuoteDetails = React.lazy(() => import('./pages/QuoteDetails'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   const notificationAlertOpen = useSelector(
@@ -24,23 +26,25 @@ function App() {
     <Fragment>
       <MainHeader />
       {notificationAlertOpen && <Notification />}
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes/all" />
-        </Route>
-        <Route path="/quotes/all" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/add" exact>
-          <AddQuote />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetails />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes/all" />
+          </Route>
+          <Route path="/quotes/all" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/add" exact>
+            <AddQuote />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetails />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Fragment>
   );
 }
